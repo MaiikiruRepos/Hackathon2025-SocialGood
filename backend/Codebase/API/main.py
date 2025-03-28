e6e80b27ef764889ddf1e86e9abaf0c887d8c13d
Steven Naliwajka <ConvexBurrito5@gmail.com>
2025-03-28 17:29:29 -0400

    update

--- backend/Codebase/API/main.py
+++ backend/Codebase/API/main.py
@@ -2,7 +2,8 @@
 from fastapi import FastAPI
 from Routes import (get_history_graph, upload_zip, get_ratings,
                     get_plant_list, get_plant_carbon, get_plant_water,
-                    get_search_data, convert_bom, generate_config)
+                    get_search_data, convert_bom, generate_config,
+                    test_api)
 
 app = FastAPI()
 
@@ -15,3 +16,4 @@ app.include_router(get_plant_water.router)
 app.include_router(get_search_data.router)
 app.include_router(convert_bom.router)
 app.include_router(generate_config.router)
+app.include_router(test_api.router)
