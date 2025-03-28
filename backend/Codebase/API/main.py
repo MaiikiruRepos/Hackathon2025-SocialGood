424ad1b9afdffc3a23f0bbe3eea5f08280f0f638
Steven Naliwajka <ConvexBurrito5@gmail.com>
2025-03-28 08:47:39 -0400

    Update

--- backend/Codebase/API/main.py
+++ backend/Codebase/API/main.py
@@ -1,8 +1,8 @@
 # app/main.py
 from fastapi import FastAPI
-from app.routes import history_graph, upload_zip
+from Routes import get_history_graph, upload_zip
 
 app = FastAPI()
 
-app.include_router(history_graph.router)
+app.include_router(get_history_graph.router)
 app.include_router(upload_zip.router)
