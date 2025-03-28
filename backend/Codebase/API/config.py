b7919fe635c0b14043313884c5da2c07228c9ccf
Maiikiru <89322078+Maiikiru@users.noreply.github.com>
2025-03-28 17:58:02 -0400

    Fix

--- backend/Codebase/API/config.py
+++ backend/Codebase/API/config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 from pathlib import Path
 from sqlalchemy import create_engine
 
-env_path = Path(__file__).resolve().parents[1] / '.env'
+env_path = Path(__file__).resolve().parent.parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
 DB_USER = os.getenv("MYSQL_USER")
-DB_PASS = os.getenv("MYSQL_USER_PASSWORD")
-DB_HOST = os.getenv("MYSQL_HOST")
+DB_PASS = os.getenv("MYSQL_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
 
 def get_engine(db_name="information_schema"):
