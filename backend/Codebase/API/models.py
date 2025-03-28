9a1a776f6544524bf91ed19ef3add9eacfe98f08
Maiikiru <89322078+Maiikiru@users.noreply.github.com>
2025-03-28 19:20:00 -0400

    add my models

--- backend/Codebase/API/models.py
+++ backend/Codebase/API/models.py
@@ -3,7 +3,15 @@ from pydantic import BaseModel
 
 class HistoryInput(BaseModel):
     googleID: str
+    timeInstance: list[str]
+
+class SingleInput(BaseModel):
+    """
+    This wrapper class gets a single googleid-timeinstance pair.
+    """
+    googleID: str
     timeInstance: str
+
 
 
 class SearchInput(BaseModel):
