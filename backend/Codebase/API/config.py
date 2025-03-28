4986fd7cad1f190a81ff8a92e0d288c40b49bfef
Maiikiru <89322078+Maiikiru@users.noreply.github.com>
2025-03-28 18:15:09 -0400

    Change to use root

--- backend/Codebase/API/config.py
+++ backend/Codebase/API/config.py
@@ -7,10 +7,9 @@ from sqlalchemy import create_engine
 env_path = Path(__file__).resolve().parent.parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
-DB_USER = os.getenv("MYSQL_USER")
-DB_PASS = os.getenv("MYSQL_PASSWORD")
+DB_PASS = os.getenv("MYSQL_ROOT_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
 
 def get_engine(db_name="information_schema"):
-    return create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{db_name}")
+    return create_engine(f"mysql+pymysql://root:{DB_PASS}@{DB_HOST}:{DB_PORT}/{db_name}")
